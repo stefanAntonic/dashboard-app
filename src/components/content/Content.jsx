8ad646b4c149bd0566dc98212ee31cacb9b9f763
davestrangelove94 <stefandantonic@gmail.com>
2022-06-05 00:46:02 +0200

    creating content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,8 +1,71 @@
 function Content() {
+  const handleClick = (e) => {
+    const el = document.getElementById(e.target.id);
+    el.classList.add("text-[#F06021]", "border-[#F06021]", "tab-bordered");
+  };
   return (
     <div className="bg-[#FDFDFD] col-span-10 ">
-      Content
+      {/* heading */}
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <h1 className="text-3xl font-normal ml-8">Analytics</h1>
+        </div>
+        <div className="flex-none">
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered input-sm rounded-full w-full max-w-xs mr-8"
+          />
+        </div>
       </div>
-  )
+      <div className="tabs flex justify-center">
+        <a className="tab font-bold " onClick={handleClick} id="today">
+          Today
+        </a>
+        <a className="tab font-bold   tab-active: tab-bordered border-[#F06021] text-[#F06021]">
+          Week
+        </a>
+        <a className="tab font-bold " onClick={handleClick} id="month">
+          Month
+        </a>
+        <a className="tab font-bold ">3Months</a>
+        <a className="tab font-bold ">6Months</a>
+        <a className="tab font-bold ">1Year</a>
+        <a className="tab font-bold ">2Years</a>
+      </div>
+
+      <div className="ml-8 mt-12">
+        <h1 className="text-2xl">Money & Selling</h1>
+        <div className="flex flex-row gap-3 pt-6">
+          <div class="form-control w-full max-w-xs">
+            <label className="label label-text font-bold">Region</label>
+            <select class="select select-bordered">
+              <option disabled selected>
+                Pick one
+              </option>
+              <option>Star Wars</option>
+              <option>Harry Potter</option>
+              <option>Lord of the Rings</option>
+              <option>Planet of the Apes</option>
+              <option>Star Trek</option>
+            </select>
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label className="label label-text font-bold">Goods category</label>
+            <select class="select select-bordered">
+              <option disabled selected>
+                Pick one
+              </option>
+              <option>Star Wars</option>
+              <option>Harry Potter</option>
+              <option>Lord of the Rings</option>
+              <option>Planet of the Apes</option>
+              <option>Star Trek</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Content
+export default Content;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,69 @@
+import { IoAnalyticsOutline, IoBagHandleOutline } from "react-icons/io5";
+import { FiTruck } from "react-icons/fi";
+import { MdOutlineReviews } from "react-icons/md";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { TbHierarchy2, TbLogout } from "react-icons/tb";
+
 function Sidebar() {
   return (
-    <div className="bg-zinc-100 col-span-2 ">
-        Sidebar</div>
-
-
-  )
+    <div className="hidden sm:block md:col-span-4 bg-zinc-100 lg:col-span-2 flex flex-col flex flex-col">;
+      <div className=" flex flex-col justify-start items-center ">
+        <h1 className="m-5 text-3xl text-[#F06021] font-serif">FloralSun</h1>
+        <div className="avatar">
+          <div className="w-24 rounded-full ">
+            <img
+              src="https://api.lorem.space/image/face?hash=92310"
+              alt="avatar"
+            />
+          </div>
+        </div>
+        <strong> Hi, Aleksandra</strong>
+      </div>
+      <ul className="flex flex-col justify-start m-7 space-y-4 pt-8">
+        <li>
+          <a className="flex flex-row gap-2 active:text-[#F06021]" href="/">
+            <IoAnalyticsOutline className="place-self-center" />
+            Analytics
+          </a>
+        </li>
+        <li>
+          <a className="flex flex-row gap-2" href="/">
+            <IoBagHandleOutline className="place-self-center" />
+            Goods
+          </a>
+        </li>
+        <li>
+          <a className="flex flex-row gap-2" href="/">
+            <FiTruck className="place-self-center" />
+            Delivery
+          </a>
+        </li>
+        <li>
+          <a className="flex flex-row gap-2" href="/">
+            <MdOutlineReviews className="place-self-center" />
+            Reviews
+          </a>
+        </li>
+        <li>
+          <a className="flex flex-row gap-2" href="/">
+            <HiOutlineUserCircle className="place-self-center" />
+            Users
+          </a>
+        </li>
+        <li>
+          <a className="flex flex-row gap-2" href="/">
+            <TbHierarchy2 className="place-self-center" />
+            Warehouse&logistic
+          </a>
+        </li>
+        <li>
+          <a className="flex flex-row pt-64 px-16" href="/">
+            <TbLogout className="place-self-center" />
+            Logout
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
-export default Sidebar
+export default Sidebar;
