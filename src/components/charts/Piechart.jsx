9481e9d1c296c716cf3c0e325662e434cdb4d2d1
davestrangelove94 <stefandantonic@gmail.com>
2022-06-06 16:09:37 +0200

    separating charts

--- src/components/charts/Piechart.jsx
+++ src/components/charts/Piechart.jsx
@@ -1,27 +1,45 @@
-import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { ChartType } from "../../enums/ChartType";
+import CustomChart from "./Chart.js";
+import { faker } from "@faker-js/faker";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+const options = {
+  responsive: true,
+  interaction: {
+    mode: "index",
+    intersect: false,
+  },
+  maintainAspectRatio: false,
+  stacked: false,
+  plugins: {},
+  scales: {},
+};
 
 const data = {
   labels: ["Mobile", "Desktop", "Tablet"],
   datasets: [
     {
-      label: "# of Votes",
-      data: [400, 610, 201],
+      label: "",
+      data: [
+        faker.datatype.number({ min: 0, max: 401 }),
+        faker.datatype.number({ min: 0, max: 611 }),
+        faker.datatype.number({ min: 0, max: 221 })
+      ],
       backgroundColor: [
         "rgb(255, 99, 132)",
         "rgb(54, 162, 235)",
         "rgb(94, 63, 190)",
       ],
-      borderColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(94, 63, 190)"],
+      borderColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(94, 63, 190)",
+      ],
       borderWidth: 1,
     },
   ],
 };
 
 function Piechart() {
-  return <Pie data={data} options={{responsive: false}} />;
+  return <CustomChart type={ChartType.Pie} data={data} options={options} />;
 }
 export default Piechart;
--- src/components/content/contentComponents/ChartLineOne.jsx
+++ src/components/content/contentComponents/ChartLineOne.jsx
@@ -2,27 +2,26 @@ import Piechart from "../../charts/Piechart";
 import Linechart from "../../charts/Linechart";
 
 function ChartLineOne() {
+  const handleClick = (e) => {
+    const el = document.getElementById(e.target.id);
+    const elements = Array.from(document.getElementsByClassName("tab"));
+    elements.forEach((e) => {
+      if (e.id !== e) {
+        e.classList.remove(
+          "text-[#F06021]",
+          "border-[#F06021]",
+          "tab-bordered"
+        );
+      }
+    });
 
-    const handleClick = (e) => {
-        const el = document.getElementById(e.target.id);
-        const elements = Array.from(document.getElementsByClassName("tab"));
-        elements.forEach((e) => {
-          if (e.id !== e) {
-            e.classList.remove(
-              "text-[#F06021]",
-              "border-[#F06021]",
-              "tab-bordered"
-            );
-          }
-        });
-    
-        el.classList.add("text-[#F06021]", "border-[#F06021]", "tab-bordered");
-      };
+    el.classList.add("text-[#F06021]", "border-[#F06021]", "tab-bordered");
+  };
 
   return (
-   <>
-   {/* Charts first line starts here */}
-   <div className=" ml-8 mt-12  flex justify-start w-64 ">
+    <>
+      {/* Charts first line starts here */}
+      <div className=" ml-8 mt-12  flex justify-start w-64 ">
         <a className="tab mr-6" id="subtitle1-price" onClick={handleClick}>
           Price($)
         </a>
@@ -30,16 +29,19 @@ function ChartLineOne() {
           Things
         </a>
       </div>
-      <div className=" grid grid-cols-4  gap-5 mt-2 place-content-evenly  pl-8 pr-8 h-60 box-border">
-        <div className=" card bg-base-100  shadow-xl col-start-1 col-end-3 ">
-          <Linechart />
+      <div className=" grid grid-cols-4  gap-5 mt-2 place-content-evenly  pl-8 pr-8 box-border">
+        <div className=" card bg-base-100  shadow-md col-start-1 col-end-3 ">
+          <div className="card-body ">
+            <Linechart />
+          </div>
         </div>
-        <div className="card bg-base-100  shadow-xl ">
-          <div className="card-body  place-self-center">
+
+        <div className="card bg-base-100  shadow-md ">
+          <div className="card-body  ">
             <Piechart />
           </div>
         </div>
-        <div className="card  bg-base-100 shadow-xl ">
+        <div className="card  bg-base-100 shadow-md">
           <div className="card-body pt-12 pl-12">
             <h2 className="font-normal">All selings:</h2>
             <h2 className="font-normal">Sale selings:</h2>
@@ -49,7 +51,7 @@ function ChartLineOne() {
           </div>
         </div>
       </div>
-   </>
-  )
+    </>
+  );
 }
-export default ChartLineOne
+export default ChartLineOne;
